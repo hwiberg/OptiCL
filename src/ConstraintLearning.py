9f950f170adbe1dba01fd20f336523b3848de69a
Holly Wiberg <holly.wiberg@gmail.com>
2021-10-26 14:10:52 -0700

    sync core files for OptiCL modeling

---
+++ src/ConstraintLearning.py
@@ -0,0 +1,436 @@
+import pandas as pd
+import numpy as np
+import math
+
+
+'''
+ContraintLearning class contains a set of functions for the
+extrapolation of constraints from a trained Optimal Classification Tree
+(with and without hyperplanes). 
+'''
+class ConstraintLearning(object):
+    '''
+    The initialization requires:
+    X: dataset without the target column
+    y: target column
+    grid: learned model.
+    '''
+    def __init__(self, X, y, learner, algorithm):
+        if not algorithm in ['iai','rf','cart','linear','svm','mlp', 'gbm']: #error!
+            raise ValueError("invalid algorithm!") 
+
+        self.__data = X
+        self.__label = y
+        self.__learner = learner
+        self.__algorithm = algorithm
+
+    def get_features_list(self):
+        '''
+        Returns the list of features that describe the dataset.
+        '''
+        return list(self.__data.columns)
+
+    def opt_data_reduction(self, datapoint, data):
+        '''
+        This function is called by data_reduction(). It checks if
+        the datapoint is within the convex hull solving an optimziation model.
+        '''
+        model = ConcreteModel()
+        model.I = RangeSet(0, data.shape[0] - 1)
+        model.J = RangeSet(0, data.shape[1] - 1)
+
+        def x_init(model, i, j):
+            return data[i][j]
+
+        def d_init(model, i):
+            return datapoint[i]
+
+        model.x = Param(model.I, model.J, initialize=x_init)
+        model.d = Param(model.J, initialize=d_init)
+        model.l = Var(model.I, domain=NonNegativeReals)
+
+        def obj_function(model):
+            return 1
+
+        model.OBJ = Objective(rule=obj_function, sense=minimize)
+
+        def constraint_rule1(model):
+            return sum(model.l[i] for i in model.I) == 1
+
+        def constraint_rule2(model, j):
+            return sum(model.l[i] * model.x[i, j] for i in model.I) == model.d[j]
+
+        model.Constraint1 = Constraint(rule=constraint_rule1)
+        model.Constraint2 = Constraint(model.J, rule=constraint_rule2)
+
+        opt = SolverFactory('cplex')
+        results = opt.solve(model)
+
+        # print('Termination condition:', results['Solver']._list[0]['Termination condition'].key)
+        if results['Solver']._list[0]['Termination condition'].key != 'optimal':
+            return True
+        else:
+            return False
+
+    def data_reduction(self):
+        '''
+        This function delete samples that are not verteces of the convex hull.
+        '''
+        data_reduced = pd.DataFrame(columns=self.__data.columns)
+        c = 0
+        while c < self.__data.shape[0]:
+            datapoint = self.__data[c:c + 1]
+            data = np.delete(self.__data, datapoint, axis=0)
+            result = self.opt_data_reduction(datapoint[0], data)
+            if result:
+                data_reduced.append(datapoint)
+            c += 1
+        return data_reduced
+
+    def mvee_approximation(self, epsilon):
+        """
+        It Finds the ellipse equation in "centre form"
+        (x-c).T * A * (x-c) <= 1 that describes the trust region.
+        It is an alternative to the convex hull.
+
+        Input: data
+               epsilon - 0.01 is a good value.
+
+
+        Output: A - (d x d) positive semi-definite matrix
+                c - (1 x d) centre of the ellipse.
+
+        """
+
+        N, d = self.__data.shape
+
+        n = d + 1.0
+
+        # Lifting of "points" so that it is centrally symmetric
+        A1 = np.column_stack((self.__data, np.ones(N))).T
+
+        err = epsilon + 1.0
+
+        # N x 1 vector where each element is 1/N
+        p = np.ones(N) / N
+
+        # While the computed solution does not satisfy the relaxed
+        # optimality conditions...
+        while err > epsilon:
+            # A1 * diag(p) * A1.T matrix
+            X = np.dot((p * A1),
+                       A1.T)
+
+            # Get diagonal of A1.T * inv(X) * A1 matrix
+            # each element: np.dot(np.dot(A1[:,1].reshape(4,1).T, la.inv(X)), A1[:,1].reshape(4,1))
+            M = np.diag(np.dot(np.dot(A1.T, np.linalg.inv(X)), A1))  # when X is not invertible?
+
+            # Get the index of the maximum value in M
+            j = np.argmax(M)
+
+            # Calculate step size for the ascent.
+            beta = (M[j] - n) / (n * (M[j] - 1.0))
+
+            # Calculate new p
+            new_p = (1 - beta) * p
+
+            # Increment the jth element of p by step_size.
+            new_p[j] += beta
+
+            # The updated error is the sum-of-square-differences
+            # between new_p and p.
+            err = np.linalg.norm(new_p - p)
+
+            # Update p
+            p = new_p
+
+        # Compute the centre of ellipse.
+        c = np.dot(p, self.__data)
+
+        # Compute the A matrix for the ellipse
+        Q = np.linalg.inv(np.dot(np.dot(self.__data.T, np.diag(p)), self.__data)
+                   - np.multiply.outer(c, c)) / d
+
+        return Q, c
+
+    # def convex_combination(self):
+    #     model = ConcreteModel()
+    #
+    #     model.I = RangeSet(0, self.__data.shape[0] - 1)
+    #     model.J = self.get_features_list()
+    #
+    #     model.l = Var(model.I, domain=NonNegativeReals)
+    #     model.x = Var(model.J, domain=Reals)  # this can be a problem because it has to be defined in the same way by the user
+    #
+    #     def constraint_rule1(model):
+    #         return sum(model.l[i] for i in model.I) == 1
+    #
+    #     def constraint_rule2(model, j):
+    #         return sum(model.l[i] * self.__data.loc[i, j] for i in model.I) == model.x[j]
+    #
+    #     model.Constraint1 = Constraint(rule=constraint_rule1)
+    #     model.Constraint2 = Constraint(model.J, rule=constraint_rule2)
+    #
+    #     return model.Constraint1, model.Constraint2
+
+    def __leaf_nodes_iai(self, class_c):
+        '''
+        It returns a list of node corresponding to the leaves of the OCT(-H)
+        '''
+        leaf_nodes = []
+        num_nodes = self.__learner.get_num_nodes()
+        '''
+        find the node_indices of the leaves
+        '''
+        if class_c == 'r':
+            for node in range(1, num_nodes + 1):
+                if self.__learner.is_leaf(node_index=node):
+                    leaf_nodes.append(node)
+        else:
+            for node in range(1, num_nodes + 1):
+                if self.__learner.is_leaf(node_index=node):
+                    if self.__learner.get_classification_label(node) == class_c:
+                        leaf_nodes.append(node)
+        return leaf_nodes
+
+    def constraint_extrapolation_iai(self, class_c):
+        '''
+        It returns a matrix with all the constraints that describe the tree.
+        When class_c is not 'r' but a number, this function returns only constraints describing
+        the class class_c.
+        When type is 'regression', this function returns constraints for each leaf.
+        the column ID represent the reference leaf.
+        IMPORTANT: (constraint structure) the sign is always <=.
+        '''
+
+        columns = ['ID'] + self.get_features_list() + ['threshold', 'prediction']
+        leaf_nodes = self.__leaf_nodes_iai(class_c)
+
+        '''
+        Initialize dataframe where:
+        ID: represent the same set of constraint that describes a leaf_nodes
+        features: coefficient for each feature of the data. If coeff = 0 the feature
+        is not in the constraints.
+        threshold: right-hand side of the constraints.
+        '''
+        constraints = pd.DataFrame(columns=columns)
+        ID = 1
+        for leaf in leaf_nodes:
+            node = leaf
+            stop = False
+            parent_node = self.__learner.get_parent(node_index=node)
+            while not stop:
+                constraint = pd.DataFrame(data=np.zeros(len(columns)).reshape(1,-1), columns=columns)
+                # sign true means that the inequality constraint is Ax<=b otherwise A>b
+                sign = True if self.__learner.get_lower_child(node_index=parent_node) == node else False
+                threshold = self.__learner.get_split_threshold(node_index=parent_node)
+                constraint['threshold'] = threshold-0.000001 if sign else -threshold
+
+                # .is_hyperplane_split is TRUE if the split is a non-orthogonal hyperplane
+                if self.__learner.is_hyperplane_split(node_index=parent_node):
+                    dict_weights = self.__learner.get_split_weights(node_index=parent_node)
+                    for key in dict_weights[0]:
+                        if sign:
+                            constraint[key] = self.__learner.get_split_weights(node_index=parent_node)[0][key]
+                        else:
+                            constraint[key] = -self.__learner.get_split_weights(node_index=parent_node)[0][key]
+                else:
+                    if sign:
+                        constraint[self.__learner.get_split_feature(node_index=parent_node)] = 1
+                    else:
+                        constraint[self.__learner.get_split_feature(node_index=parent_node)] = -1
+                constraint['ID'] = ID
+                if class_c == 'r':
+                    constraint['prediction'] = self.__learner.get_regression_constant(leaf)
+                else:
+                    constraint['prediction'] = class_c
+
+                constraints = constraints.append(constraint)
+
+                node = parent_node
+                # check if the parent_node is a root node
+                if node == 1:
+                    stop = True
+                else:
+                    parent_node = self.__learner.get_parent(node_index=node)
+            ID += 1
+
+        return constraints
+
+    def __find_path_skTree(self, node_numb, path, leaf, children_left, children_right):
+        '''
+        This function is used to find the path of nodes that are visited before reaching a leaf
+        '''
+        path.append(node_numb)
+        if node_numb == leaf:
+            return True
+        left = False
+        right = False
+        if (children_left[node_numb] != -1):
+            left = self.__find_path_skTree(children_left[node_numb], path, leaf, children_left, children_right)
+        if (children_right[node_numb] != -1):
+            right = self.__find_path_skTree(children_right[node_numb], path, leaf, children_left, children_right)
+        if left or right:
+            return True
+        path.remove(node_numb)
+        return False
+
+    def __get_rule_skTree(self, leaf, path, column_names, columns, ID, type_tree, children_left, feature, threshold):
+        '''
+        This functions transform the list of nodes composing a path into a set of constraints
+        '''
+        constraints_leaf = pd.DataFrame(columns=columns)
+        for index, node in enumerate(path):
+            constraint = pd.DataFrame(data=np.zeros(len(columns)).reshape(1, -1), columns=columns)
+            # We check if we are not in the leaf
+            if node != leaf:
+                # Do we go under or over the threshold ?
+                if (children_left[node] == path[index + 1]):
+                    constraint[column_names[feature[node]]] = 1
+                    constraint['threshold'] = threshold[node]
+                else:
+                    constraint[column_names[feature[node]]] = -1
+                    constraint['threshold'] = -(threshold[node] + 0.000001)
+                constraint['ID'] = ID
+                constraints_leaf = constraints_leaf.append(constraint)
+        return constraints_leaf
+
+    def constraint_extrapolation_skTree(self, class_c):
+        '''
+        :param class_c: either r: regression or c: classification
+        :return: set of constraints that describe each leaf of the tree.
+        Constraints with the same ID describe the same leaf and must be satisfied together.
+        '''
+        children_left = self.__learner.tree_.children_left
+        children_right = self.__learner.tree_.children_right
+        feature = self.__learner.tree_.feature
+        threshold = self.__learner.tree_.threshold
+
+        # Leaves
+        leave_id = self.__learner.apply(self.__data)
+
+        columns = ['ID'] + [feature for feature in self.get_features_list()] + ['threshold', 'prediction']
+        constraints = pd.DataFrame(columns=columns)
+        for i, leaf in enumerate(np.unique(leave_id)):
+            path_leaf = []
+            self.__find_path_skTree(0, path_leaf, leaf, children_left, children_right)
+            constraints_leaf = self.__get_rule_skTree(leaf, path_leaf, self.get_features_list(), columns, i + 1, class_c, children_left, feature, threshold)
+            if class_c == 'r':
+                constraints_leaf['prediction'] = self.__learner.tree_.value[leaf].item()
+            else:
+                constraints_leaf['prediction'] = np.argmax(self.__learner.tree_.value[leaf])
+                # constraints_leaf['prediction'] = np.round(self.__learner.tree_.value[leaf].item())
+            constraints = constraints.append(constraints_leaf)
+
+        return constraints
+
+    def constraint_extrapolation_SVM(self, class_c):
+        '''
+        :return: constraint: it has the following structure: Coeff*x+intercept >= 0
+        '''
+        columns = [feature for feature in self.get_features_list()]
+        constraint = pd.DataFrame(data=[self.__learner.coef_], columns=columns)
+        constraint['intercept'] = self.__learner.intercept_
+
+        return constraint
+
+    def constraint_extrapolation_skRF(self, class_c):
+        columns = ['Tree_id', 'ID'] + [feature for feature in self.get_features_list()] + ['threshold', 'prediction']
+        constraints = pd.DataFrame(columns=columns)
+        for tree_id, tree in enumerate(self.__learner):
+            children_left = tree.tree_.children_left
+            children_right = tree.tree_.children_right
+            feature = tree.tree_.feature
+            threshold = tree.tree_.threshold
+
+            # Leaves
+            leave_id = tree.apply(self.__data)
+
+            for i, leaf in enumerate(np.unique(leave_id)):
+                path_leaf = []
+                self.__find_path_skTree(0, path_leaf, leaf, children_left, children_right)
+                constraints_leaf = self.__get_rule_skTree(leaf, path_leaf, self.get_features_list(), columns[:-1], i + 1, class_c, children_left, feature, threshold)
+                constraints_leaf['Tree_id'] = tree_id
+                if class_c == 'r':
+                    # print(tree.tree_.value[leaf])
+                    constraints_leaf['prediction'] = tree.tree_.value[leaf].item()
+                else:
+                    constraints_leaf['prediction'] = np.argmax(tree.tree_.value[leaf])
+                constraints = constraints.append(constraints_leaf)
+
+        return constraints
+
+    def constraint_extrapolation_skGBM(self, class_c):
+        columns = ['Tree_id', 'ID'] + [feature for feature in self.get_features_list()] + ['threshold', 'prediction', 'initial_prediction', 'learning_rate']
+        constraints = pd.DataFrame(columns=columns)
+        for tree_id, tree_array in enumerate(self.__learner.estimators_):
+            tree = tree_array.item()
+            children_left = tree.tree_.children_left
+            children_right = tree.tree_.children_right
+            feature = tree.tree_.feature
+            threshold = tree.tree_.threshold
+
+            # Leaves
+            leave_id = tree.apply(self.__data)
+
+            for i, leaf in enumerate(np.unique(leave_id)):
+                path_leaf = []
+                self.__find_path_skTree(0, path_leaf, leaf, children_left, children_right)
+                constraints_leaf = self.__get_rule_skTree(leaf, path_leaf, self.get_features_list(), columns[:-1], i + 1, class_c, children_left, feature, threshold)
+                constraints_leaf['Tree_id'] = tree_id
+                if class_c == 'r':
+                    # print(tree.tree_.value[leaf])
+                    constraints_leaf['prediction'] = tree.tree_.value[leaf].item()
+                    constraints_leaf['initial_prediction'] = self.__learner.init_.constant_.item()
+                    constraints_leaf['learning_rate'] = self.__learner.learning_rate
+                else:
+                    constraints_leaf['prediction'] = tree.tree_.value[leaf].item()
+                    constraints_leaf['initial_prediction'] = 0
+                    constraints_leaf['learning_rate'] = self.__learner.learning_rate
+                constraints = constraints.append(constraints_leaf)
+        return constraints
+
+    def constraint_extrapolation_skEN(self, class_c):
+        '''
+        :return: constraint: prediction follows the structure Coeff*x+intercept
+        '''
+        ## Assume a regression model
+        assert class_c == 'r'
+        columns = [feature for feature in self.get_features_list()]
+        constraint = pd.DataFrame(data=[self.__learner.coef_], columns=columns)
+        constraint['intercept'] = self.__learner.intercept_
+
+        return constraint
+
+    def __extract_layer(self, l):
+        df_sub = pd.DataFrame(self.__learner.coefs_[l].transpose()).add_prefix('node_')
+        df_sub['intercept'] = self.__learner.intercepts_[l]
+        df_sub['layer'] = l
+        df_sub['node'] = range(len(df_sub))
+        return df_sub
+
+    def constraint_extrapolation_MLP(self, class_c):
+        assert class_c == 'r'
+        n_layers = len(self.__learner.coefs_)
+        constraints = pd.concat([self.__extract_layer(l) for l in range(n_layers)],axis=0)
+        cols_to_move = ['intercept', 'layer', 'node']
+        constraints = constraints[cols_to_move + [col for col in constraints.columns if col not in cols_to_move]]
+        return constraints
+
+
+    def constraint_extrapolation(self, class_c):
+        if self.__algorithm == "iai":
+            constraints = self.constraint_extrapolation_iai(class_c)
+        elif self.__algorithm == "cart":
+            constraints = self.constraint_extrapolation_skTree(class_c)
+        elif self.__algorithm == "rf":
+            constraints = self.constraint_extrapolation_skRF(class_c)
+        elif self.__algorithm == "gbm":
+            constraints = self.constraint_extrapolation_skGBM(class_c)
+        elif self.__algorithm == "linear":
+            constraints = self.constraint_extrapolation_skEN(class_c)
+        elif self.__algorithm == "svm":
+            constraints = self.constraint_extrapolation_SVM(class_c)
+        elif self.__algorithm == "mlp":
+            constraints = self.constraint_extrapolation_MLP(class_c)
+        return constraints
