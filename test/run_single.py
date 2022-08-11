6581a904b5946c9f27ab84cca79fde6a3123e744
Holly Wiberg <holly.wiberg@gmail.com>
2022-08-11 08:26:09 -0400

    batch remaining

--- test/run_single.py
+++ test/run_single.py
@@ -25,9 +25,14 @@ param_list = list(itertools.product(*[alg_list, bs_list, violation_list]))
 # alg, bs, viol_rule = param_list[idx]
 # df = run_experiment(alg, bs, viol_rule, n_iterations = 100)
 
-failed_ids = [55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67,
- 68, 69, 70, 71, 72, 73, 74, 115, 116, 117, 118, 119, 120, 121, 122, 
- 123, 124, 140, 144, 145, 147, 148, 149]
+# failed_ids = [55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67,
+#  68, 69, 70, 71, 72, 73, 74, 115, 116, 117, 118, 119, 120, 121, 122, 
+#  123, 124, 140, 144, 145, 147, 148, 149]
+
+ failed_ids_v2 = [140, 144, 145, 147, 148, 149, 115, 116, 
+ 	117, 118, 119, 120, 121, 122, 123, 124]
+
+ # failed_ids_gbm = [62, 63, 64, 65, 66, 67, 68, 69, 70, 71, 72, 73, 74,]
 
 # patch_list = list(itertools.product(*[failed_ids, range(100)]))
 # idx = int(sys.argv[1])
@@ -36,7 +41,7 @@ failed_ids = [55, 56, 57, 58, 59, 60, 61, 62, 63, 64, 65, 66, 67,
 # df = run_experiment(alg, bs, viol_rule, fixed_seed = seed)
 
 seed = int(sys.argv[1])
-for idx in failed_ids:
+for idx in failed_ids_v2:
 	print("Failed ID = %d" % idx)
 	alg, bs, viol_rule = param_list[idx]
 	df = run_experiment(alg, bs, viol_rule, fixed_seed = seed)
